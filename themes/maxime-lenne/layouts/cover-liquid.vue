71d68c77af42698815599ea19b3e27011286e138
Maxime Lenne <hello@maxime-lenne.fr>
2026-06-24 22:57:30 +0200

    ✨ new layout

---
+++ themes/maxime-lenne/layouts/cover-liquid.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import LiquidChrome from '../components/LiquidChrome.vue'
+
+withDefaults(
+  defineProps<{
+    interactive?: boolean
+  }>(),
+  {
+    interactive: true,
+  },
+)
+</script>
+
+
+<template>
+  <div class="slidev-layout cover" :style="style">
+    <LiquidChrome class="section-liquid-bg" :interactive="interactive" />
+    <div class="my-auto w-full text-center">
+      <slot />
+    </div>
+  </div>
+</template>
